c21accf5b5405959a95c50189a131749d7870012
Keita Sakurai <keitasakuraims@gmail.com>
2024-03-07 16:15:24 +0900

    Added codes optimized with optuna

--- chapter2/list_2_12_rf.py
+++ chapter2/list_2_12_rf.py
@@ -4,18 +4,18 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 
 
-# データのダウンロードと前処理
+# Loading data and preprocessing（データのダウンロードと前処理）
 data = fetch_openml(name="adult")
 X = pd.get_dummies(data["data"])
 y = [1 if d == ">50K" else 0 for d in data["target"]]
 
-# 機械学習モデルの初期化
+# Initialized ML model（機械学習モデルの初期化）
 clf = RandomForestClassifier(
-    max_depth=8,  # ハイパーパラメータ
-    min_samples_split=0.5,  # ハイパーパラメータ
+    max_depth=8,  # Hyperparameter
+    min_samples_split=0.5,  # Hyperparameter
 )
 
-# 交差検証による評価
+# Evaluating the accuracy by Cross Validation（交差検証による評価）
 score = cross_val_score(clf, X, y, cv=3)
 accuracy = score.mean()
 print(f"Accuracy: {accuracy}")
