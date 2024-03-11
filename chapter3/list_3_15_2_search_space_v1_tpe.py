71b5547dbd9946b0b7855aaa7274a0fbc677d140
Keita Sakurai <keitasakuraims@gmail.com>
2024-03-11 12:21:40 +0900

    search space v2, v3 / plot optimization history

---
+++ chapter3/list_3_15_2_search_space_v1_tpe.py
@@ -0,0 +1,9 @@
+import optuna
+from optuna.visualization import plot_optimization_history
+
+study = optuna.load_study(
+    study_name="ch3-lightgbm-search-space-v1-tpe",
+    storage="sqlite:///optuna.db",
+)
+
+plot_optimization_history(study).show()
